707db7edde6b3cdf7832fd889cef3811d70c8612
Felipe Gallassi <felipegallassi@gmail.com>
2022-03-22 18:37:02 -0300

    requisito 4 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+const ImageAdress = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Testa o componente NotFound.js', () => {
+  it('deveria ter um heading h2 com o texto Page requested not found 😭', () => {
+    const { customHistory } = renderWithRouter(<App />);
+    customHistory.push('/asjkbasja');
+    const NotFound = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(NotFound).toBeInTheDocument();
+    const image = screen.getByAltText(/Pikachu crying because/i);
+    expect(image).toHaveAttribute('src', ImageAdress);
+  });
+});
